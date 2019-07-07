533d0bb72c268c678292035c945c488e55121c59
Alejandro Druetta <aledruetta@gmail.com>
2019-07-07 17:52:58 -0300

    Insertion Sort na lista optimizada

---
+++ Estrutura_de_Dados/aula4_lista_linear_sequencial_optimizada/cpp_version/LinearSeqList.hpp
@@ -0,0 +1,135 @@
+/*
+ * UNIVESP - ENG. DA COMPUTAÇÃO
+ *
+ * LISTA LINEAR SEQUENCIAL OPTIMIZADA (versão C++)
+ *
+ * Arquivo:    LinearSeqList.hpp
+ * Disciplina: ESTRUTURA DE DADOS
+ * Bimestre:   8vo
+ * Autor:      Alejandro Druetta
+ * Data:       06/07/2019
+ */
+
+
+using namespace std;
+const int MAX = 50;                 // Quantidade máxima de registros
+
+
+class LinearSeqList
+/*
+ * Classe: LinearSeqList
+ *
+ * A classe LinearSeqList é uma lista linear sequencial
+ * de elementos do tipo Register.
+ */
+{
+        int len;
+        Register regs[MAX+1];
+
+    public:
+
+        LinearSeqList();
+        void sortIns();                 // Insertion sort
+        void sortBub();                 // Bubble sort
+        bool append(Register reg);
+        bool insert(Register reg, int pos);
+        int find(int key);
+        bool del(int key);
+        void show();
+};
+
+LinearSeqList::LinearSeqList()
+/*
+ * Construtor da Lista.
+ */
+{
+    len = 0;
+}
+
+void LinearSeqList::show()
+/*
+ * Imprime uma representação da Lista no formato:
+ * List: [ 2 7 23 ] Length: 3
+ */
+{
+    cout << "List: [ ";
+    for (int i=0; i<len; i++)
+    {
+        cout << regs[i].getKey() << " ";
+    }
+    cout << "] Length: " << len << endl;
+}
+
+void LinearSeqList::sortIns()
+/*
+ * Ordena a lista usando o algoritmo insertion sort.
+ */
+{
+    for (int i=1; i<len; i++)
+    {
+        Register tmp = regs[i];
+        int j = i - 1;
+        while (regs[j].getKey() > tmp.getKey() && j >= 0)
+        {
+            regs[j+1] = regs[j];
+            j--;
+        }
+        regs[j+1] = tmp;
+    }
+}
+
+void LinearSeqList::sortBub()
+/*
+ * Ordena a lista usando o algoritmo bubble sort.
+ */
+{
+}
+
+bool LinearSeqList::append(Register reg)
+/*
+ * Adiciona um registro no final da lista se a lista não estiver cheia.
+ */
+{
+    if (len >= MAX) return false;
+    regs[len] = reg;
+    len++;
+    return true;
+}
+
+bool LinearSeqList::insert(Register reg, int pos)
+/*
+ * Insere um registro em uma posição determinada se a lista não
+ * estiver cheia e a posição for válida.
+ */
+{
+    if (len >= MAX || pos < 0 || pos > len-1) return false;
+    for (int i=len; i>pos; i--) regs[i] = regs[i-1];
+    regs[pos] = reg;
+    len++;
+    return true;
+}
+
+int LinearSeqList::find(int key)
+/*
+ * Procura uma chave usando o algoritmo de busca por sentinela.
+ */
+{
+    int pos = 0;
+    regs[len] = Register(key);
+    while (regs[pos].getKey() != key) pos++;
+    if (pos == len) return -1;
+    return pos;
+}
+
+bool LinearSeqList::del(int key)
+/*
+ * Deleta um elemento da lista procurado pela chave.
+ */
+{
+    int pos = find(key);
+    if (pos < 0) return false;
+    for (int i=pos; i<len-1; i++) regs[i] = regs[i+1];
+    len--;
+    return true;
+}
+
---
+++ Estrutura_de_Dados/aula4_lista_linear_sequencial_optimizada/cpp_version/main.cpp
@@ -0,0 +1,135 @@
+/*
+ * UNIVESP - ENG. DA COMPUTAÇÃO
+ *
+ * LISTA LINEAR SEQUENCIAL OPTIMIZADA (versão C++)
+ *
+ * Arquivo:    main.cpp
+ * Disciplina: ESTRUTURA DE DADOS
+ * Bimestre:   8vo
+ * Autor:      Alejandro Druetta
+ * Data:       05/07/2019
+ */
+
+#include <iostream>
+#include <cstdlib>                  // rand, srand
+#include <ctime>                    // time
+#include <limits>
+#include "Register.hpp"
+#include "LinearSeqList.hpp"
+
+void printMenu();
+void flushCin();
+
+
+int main()
+/*
+ * Função: main.
+ */
+{
+    LinearSeqList list;
+    srand(time(nullptr));
+
+    for (int i=0; i<10; i++)
+    {
+        Register reg(rand() % 100);
+        if (!list.append(reg)) cout << "Não há mais espaço na lista!" << endl;
+    }
+    list.show();
+
+    int key;
+    int pos;
+    char op = 'm';
+
+    do {
+
+        switch (op)
+        {
+            case 'a':
+            // Adicionar
+            {
+                cout << "Chave? ";
+                cin >> key;
+                flushCin();
+                Register reg(key);
+                list.append(reg);
+                list.show();
+                break;
+            }
+            case 'i':
+            // Inserir
+            {
+                cout << "Chave? ";
+                cin >> key;
+                flushCin();
+                Register reg(key);
+                cout << "Posição? ";
+                cin >> pos;
+                flushCin();
+                list.insert(reg, pos);
+                list.show();
+                break;
+            }
+            case 'p':
+            // Procurar
+            {
+                cout << "Chave? ";
+                cin >> key;
+                cout << "Posição: " << list.find(key) << endl;
+                list.show();
+                break;
+            }
+            case 'd':
+            // Deletar
+            {
+                cout << "Chave? ";
+                cin >> key;
+                flushCin();
+                list.del(key);
+                list.show();
+                break;
+            }
+            case 'v':
+            // Visualizar
+                list.show();
+                break;
+            case 'o':
+            // Ordenar por Insertion Sort
+                list.sortIns();
+                break;
+            case 'm':
+            // Menu
+                printMenu();
+                break;
+            default:
+                cout << "Opção inválida!" << endl;
+        }
+
+        cout << "\nOpção: ";
+        cin.get(op);
+        flushCin();
+
+    } while (op != 's');
+
+    return 0;
+}
+
+void printMenu()
+{
+        cout << "\nMenu:"        << endl
+             << "=====\n"        << endl
+             << "a. Adicionar"   << endl
+             << "i. Inserir"     << endl
+             << "p. Procurar"    << endl
+             << "d. Deletar"     << endl
+             << "v. Visualizar"  << endl
+             << "o. Ordenar"     << endl
+             << "m. Menu"        << endl
+             << "s. Sair"        << endl;
+}
+
+void flushCin()
+{
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(),'\n');
+}
+
