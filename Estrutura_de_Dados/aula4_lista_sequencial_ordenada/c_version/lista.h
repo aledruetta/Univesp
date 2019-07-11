607054ff2e2b135bbe50e6230bd5fb5fe1de6d90
Alejandro Druetta <aledruetta@gmail.com>
2019-07-11 17:56:05 -0300

    header comments

--- Estrutura_de_Dados/aula4_lista_sequencial_ordenada/c_version/lista.h
+++ Estrutura_de_Dados/aula4_lista_sequencial_ordenada/c_version/lista.h
@@ -34,18 +34,18 @@ typedef struct
 } LIST;
 
 
-void initList (LIST * list);
-void showList (LIST list);
-int lenList (LIST list);
-int findSeqList (LIST list, KEY key);
-int findSentList (LIST * list, KEY key);
-int findBinList (LIST list, KEY key);
-bool addList (LIST * list, REGISTER reg);
-bool delList (LIST * list, KEY key);
-void sortInsList (LIST * list);
-void sortBinList (LIST * list);
-bool insPosList (LIST * list, REGISTER reg, int pos);
-bool insOrdList (LIST * list, REGISTER reg);
+void initList (LIST * list);                            // Inicializar
+void showList (LIST list);                              // Mostrar
+int lenList (LIST list);                                // Cumprimento
+int findSeqList (LIST list, KEY key);                   // Busca Sequencial
+int findSentList (LIST * list, KEY key);                // Busca por Sentinela
+int findBinList (LIST list, KEY key);                   // Busca Binária
+bool addList (LIST * list, REGISTER reg);               // Adicionar
+bool delList (LIST * list, KEY key);                    // Deletar
+void sortInsList (LIST * list);                         // Insertion Sort
+void sortBinList (LIST * list);                         // Binary Sort
+bool insPosList (LIST * list, REGISTER reg, int pos);   // Inserir Posicional
+bool insOrdList (LIST * list, REGISTER reg);            // Inserir Ordenada
 
 #endif
 
