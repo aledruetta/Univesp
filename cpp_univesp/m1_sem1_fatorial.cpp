85950b75f2d2608a6fb69805fb1b4d4a5050d36e
Alejandro Druetta <aledruetta@gmail.com>
2019-07-23 15:56:03 -0300

    fatorial loops

---
+++ cpp_univesp/m1_sem1_fatorial.cpp
@@ -0,0 +1,54 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  m1_sem1_fatorial.cpp
+ *
+ *    Description:  Cálculo do fatorial de um número inteiro
+ *
+ *        Version:  1.0
+ *        Created:  23/07/2019 15:28:09
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Alejandro Druetta
+ *   Organization:  Eng. Computação - Univesp
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main ()
+{
+    unsigned num, numd, fac;
+    cout << "\nNúmero: ", cin >> num;
+    numd = num;
+
+    /* *
+     * Loop FOR
+     * */
+    fac = 1;
+    for (int i=1; i<=num; i++) fac *= i;
+    cout << setw(24) << "Fatorial loop FOR: " << fac << endl;
+
+    /* *
+     * Loop WHILE
+     * */
+    fac = 1;
+    while (num > 0) fac *= num--;
+    cout << setw(24) << "Fatorial loop WHILE: " << fac << endl;
+
+    /* *
+     * Loop DO/WHILE
+     * */
+    fac = 1;
+    do fac *= numd--;
+    while (numd > 0);
+    cout << setw(24) << "Fatorial loop DO/WHILE: " << fac << "\n"
+         << endl;
+
+    return 0;
+}
+
