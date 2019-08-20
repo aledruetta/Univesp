aa370148bba468ff876b6feeda24edfd1471c43b
Alejandro Druetta <aledruetta@gmail.com>
2019-08-20 10:31:38 -0300

    Lista Linear: inserir

---
+++ Estruturas_de_Dados/Lista_Linear/Sequencial/lista_linear_sequencial.h
@@ -0,0 +1,31 @@
+/*
+ * File: Lista_Linear/Sequencial/lista.h
+ * @author: Alejandro Druetta
+ */
+
+#ifndef LISTA_LINEAR_SEQUENCIAL_H
+#define LISTA_LINEAR_SEQUENCIAL_H
+
+#include <stdbool.h>
+
+#define MAX 5
+
+typedef int Chave;
+
+typedef struct
+{
+    Chave chave;
+} Registro;
+
+typedef struct
+{
+    Registro regs[MAX];
+    unsigned tamanho;
+} LLSeq;
+
+void inicializar_llseq (LLSeq*);
+bool adicionar_llseq (LLSeq*, const Registro*);
+void mostrar_llseq (const LLSeq*);
+bool inserir_llseq (LLSeq*, const Registro*, unsigned);
+
+#endif /* LISTA_LINEAR_SEQUENCIAL_H */
--- Estruturas_de_Dados/Lista_Linear/Sequencial/main.c
+++ Estruturas_de_Dados/Lista_Linear/Sequencial/main.c
@@ -4,29 +4,50 @@
  */
 
 #include <stdio.h>
-#include "lista_linear.h"
+#include <stdbool.h>
+#include "lista_linear_sequencial.h"
 
 int main ()
 {
     // inicializar lista
-    ListaLinear lista;
-    inicializar_llin (&lista);
+    LLSeq lista;
+    inicializar_llseq (&lista);
     printf ("Tamanho máximo: %d elementos\n", MAX);
 
     // adicionar elementos à lista
-    Chave chaves[MAX+1] = {5, 3, 0, 9, 8};
+    Chave chaves1[MAX+1] = {5, 3, 0, 9, 8};
     for (unsigned i=0; i<MAX+1; i++) {
-        Registro registro = {chaves[i]};
+        Registro registro = {chaves1[i]};
         printf ("Adicionando: %d\n", registro.chave);
-        bool adicionado = adicionar_llin (&lista, &registro);
+        bool adicionado = adicionar_llseq (&lista, &registro);
         if (!adicionado)
             printf ("Erro: Lista cheia!\n");
     }
 
     // mostrar lista
-    mostrar_llin (&lista);
+    mostrar_llseq (&lista);
 
     // inserir elemento na lista
+    Registro registro = {2};
+    bool inserido = inserir_llseq (&lista, &registro, 2);
+    if (!inserido)
+        printf ("Erro: Lista cheia ou ídice inválido!\n");
+    mostrar_llseq (&lista);
+
+    inicializar_llseq (&lista);
+    Chave chaves2[MAX-1] = {5, 3, 0, 9};
+    for (unsigned i=0; i<MAX-1; i++) {
+        Registro registro = {chaves2[i]};
+        printf ("Adicionando: %d\n", registro.chave);
+        bool adicionado = adicionar_llseq (&lista, &registro);
+        if (!adicionado)
+            printf ("Erro: Lista cheia!\n");
+    }
+    inserido = inserir_llseq (&lista, &registro, 3);
+    if (!inserido)
+        printf ("Erro: Lista cheia ou ídice inválido!\n");
+    mostrar_llseq (&lista);
+
     // procurar elemento na lista
     // remover elemento da lista
 
