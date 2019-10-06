14925bef3fb7fcd2444b6c4640085796e7a7abe4
Alejandro Druetta <aledruetta@gmail.com>
2019-10-06 15:18:28 -0300

    fix setup server

--- Projeto_Integrador/projeto_integrador_4/setup_server.sh
+++ Projeto_Integrador/projeto_integrador_4/setup_server.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-# uso:
-# sudo bash ./setup_server.sh <url>
-
-set -ex
+# usage:
+# sudo bash ./setup_server.sh <ip>
+# set -ex
+set -e
 
 VIRT_SERV="/etc/nginx/sites-available/pi4"
 NEW_URL="$1"
+URL_PATTERN="\([0-9]\{1,3\}.\)\{3\}[0-9]\{1,3\}"
 
 if [[ -e "$VIRT_SERV" ]]; then
-	sed -i -e "s/server_name \([0-9]\{1,3\}.\)\{3\}[0-9]\{1,3\}/server_name $NEW_URL/" "$VIRT_SERV"
+	sed -i -e "s/server_name $URL_PATTERN/server_name $NEW_URL/" "$VIRT_SERV"
 else
 	exit 1;
 fi
