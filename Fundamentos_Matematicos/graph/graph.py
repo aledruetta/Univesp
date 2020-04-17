e519d4ab06f19cd42c03dcefb86c7f45673c8f81
Alejandro Druetta <aledruetta@gmail.com>
2020-04-17 17:08:49 -0300

    Sorts vertices and edges. Comments

--- Fundamentos_Matematicos/graph/graph.py
+++ Fundamentos_Matematicos/graph/graph.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 # import numpy as np
-from graph_exceptions import *
+from graph_exceptions import (
+        VertexNotExistsError,
+        IsNotSimpleGraphError)
 
 
 class Graph:
+    """ This class represents a generic graph. It's has a set
+        of vertices and a list of edges (a 2-len list of vertices).
+    """
     def __init__(self, V, E):
         self.V = V          # list of vertices
         self.E = E          # list of edges
@@ -13,14 +18,24 @@ class Graph:
         self._check_edges()
 
     def _check_vertices(self):
+        """ Deletes duplicated vertices and checks if they are
+            are integers.
+        """
         self.V = list(set(self.V))
+        self.V.sort()
+
         for vert in self.V:
             if not isinstance(vert, int):
                 raise TypeError
 
     def _check_edges(self):
+        """ Checks if every edge connect two vertices and
+            if they are tuples.
+        """
+        self.E.sort()
+
         for edge in self.E:
-            if not isinstance(edge, tuple) or len(edge) != 2:
+            if not isinstance(edge, list) or len(edge) != 2:
                 raise TypeError
             elif (
                     edge[0] not in self.V or
@@ -32,6 +47,8 @@ class Graph:
 
 
 class SimpleGraph(Graph):
+    """
+    """
     def __init__(self, V, E):
         super().__init__(V, E)
 
