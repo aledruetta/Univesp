eb4a838d336d4e63ff524bcf5194f1b428aa9993
Alejandro Druetta <aledruetta@gmail.com>
2020-05-11 16:38:37 -0300

    Semana 1 Apoio - Questão 1

---
+++ POO/univesp_POO_semana1/src/univesp_POO_semana1/Data.java
@@ -0,0 +1,60 @@
+package univesp_POO_semana1;
+
+public class Data {
+	int dia;
+	int mes;
+	int ano;
+	
+	public Data(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+	
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+	
+	public int getDia() {
+		return this.dia;
+	}
+	
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+	
+	public int getMes() {
+		return this.mes;
+	}
+	
+	public int getAno() {
+		return this.ano;
+	}
+	
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+	public String toString() {
+		return this.dia + "/" + this.mes + "/" + this.ano;
+	}
+	
+	public void trocarCom(Data data) {
+		Data tmp = null;
+
+		try {
+			tmp = (Data) data.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+
+		data.setDia(this.dia);
+		data.setMes(this.mes);
+		data.setAno(this.ano);
+
+		this.dia = tmp.getDia();
+		this.mes = tmp.getMes();
+		this.ano = tmp.getAno();
+	}
+
+}
---
+++ POO/univesp_POO_semana1/src/univesp_POO_semana1/UnivespPooSemana1.java
@@ -0,0 +1,20 @@
+package univesp_POO_semana1;
+
+public class UnivespPooSemana1 {
+
+	public static void main(String[] args) {
+		
+		Data dataInicio = new Data(1, 1, 2019);
+		Data dataFinal = new Data(31, 12, 2019);
+		
+		System.out.println(dataInicio);
+		System.out.println(dataFinal);
+		
+		dataInicio.trocarCom(dataFinal);
+		
+		System.out.println(dataInicio);
+		System.out.println(dataFinal);
+
+	}
+
+}
