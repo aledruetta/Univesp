a331cef9bf131bb4e33c962b871f362313d505a2
Alejandro Druetta <aledruetta@gmail.com>
2020-07-23 18:39:01 -0300

    histograma vertical

---
+++ C_Kernighan/09_histo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define NCHARS 25
+
+main()
+{
+    char c;
+    int i, j, histo[NCHARS];
+
+    for (i=0; i<NCHARS; i++)
+        histo[i] = 0;
+
+    printf("Frase: ");
+    while ((c = getchar()) != '\n') {
+        if (c >= 'a' && c <= 'z') {
+            ++histo[c-'a'];
+        }
+    }
+
+    for (i=0; i<NCHARS; i++) {
+        for (j=0; j<histo[i]; j++)
+            printf("%c", i + 'a');
+        if (histo[i] != 0)
+            printf("\n");
+    }
+}
