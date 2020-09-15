f93d72769ca5f209e3eb59241513ce080f5e7404
Alejandro Druetta <aledruetta@gmail.com>
2020-09-15 10:15:20 -0300

    Exercise 1-17

--- Kernighan_C/exercise_01_17.c
+++ Kernighan_C/exercise_01_17.c
@@ -1,9 +1,40 @@
 /* Exercise 1-17 */
 
 #include <stdio.h>
+#define MAXBUF 1000
+#define MAXLINE 80
+
+int custom_getline(char line[], int max);
 
 int main()
 {
+    char line[MAXBUF];
+    int len;
+
+    while ((len = custom_getline(line, MAXBUF)) > 0) {
+        if (len > MAXLINE) {
+            printf("%s", line);
+            printf("%d\n", len);
+        }
+    }
 
     return 0;
+}
+
+int custom_getline(char line[], int max)
+{
+    char c;
+    int len = 0;
+
+    while ((c = getchar()) != EOF && c != '\n' && len < max) {
+        line[len] = c;
+        len++;
+    }
+    if (c == '\n') {
+        line[len] = '\n';
+        len++;
+    }
+    line[len] = '\0';
+
+    return len;
 }
