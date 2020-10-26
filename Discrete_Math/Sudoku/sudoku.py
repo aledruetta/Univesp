55285983f4ec649bcd311c7c4734c46ac62b8286
Alejandro Druetta <aledruetta@gmail.com>
2020-10-26 10:43:55 -0300

    fix dunder str

--- Discrete_Math/Sudoku/sudoku.py
+++ Discrete_Math/Sudoku/sudoku.py
@@ -1,25 +1,44 @@
 #!/usr/bin/env python3
 
 import numpy as np
-from random import choice
 
 
 class Sudoku:
     def __init__(self):
         self.matrix = self.create_matrix()
 
-    def get_block(self, num):
-        num -= 1
-        row_start = num - (num % 3)
+    def get_block(self, n):
+        """ Blocks are numberd from 1 to 9
+            Return n numberd block
+        """
+        n -= 1
+        row_start = n - (n % 3)
         row_end = row_start + 3
-        col_start = 3 * (num % 3)
+        col_start = 3 * (n % 3)
         col_end = col_start + 3
         return self.matrix[row_start:row_end, col_start:col_end]
 
     def create_matrix(self):
-        # matrix = np.empty((9, 9))
-        # matrix[:] = np.nan
+        matrix = np.empty((9, 9))
+        matrix[:] = np.nan
         return np.random.randint(10, size=(9, 9))
+        # return matrix
 
-    def __repr__(self):
-        return str(self.matrix)
+    def __str__(self):
+        seph = 3 * "+-------" + "+\n"
+        output = ""
+        for i in range(9):
+            if i % 3 == 0:
+                output += seph
+            for j in range(9):
+                if j % 3 == 0:
+                    output += "| "
+                output += str(self.matrix[i, j]) + " "
+            output += "|\n"
+        output += seph
+        return output
+
+
+if __name__ == "__main__":
+    s = Sudoku()
+    print(s)
