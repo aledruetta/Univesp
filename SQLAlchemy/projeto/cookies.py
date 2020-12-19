ac725ae794f21f2d69e0226e3bd3bf21d47bf0f1
Alejandro Druetta <aledruetta@gmail.com>
2020-12-19 11:30:43 -0300

    ADD: More tables

--- SQLAlchemy/projeto/cookies.py
+++ SQLAlchemy/projeto/cookies.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy import Table, Column, ForeignKey, Index
-from sqlalchemy import Integer, String, Numeric, DateTime
+from sqlalchemy import Integer, String, Numeric, DateTime, Boolean
 from sqlalchemy import PrimaryKeyConstraint, UniqueConstraint, CheckConstraint
 
 # engine = create_engine('postgresql+psycopg2://sqlalchemy'
@@ -33,14 +33,21 @@ users = Table(
 )
 
 cookies = Table(
-    'cookies',
-    metadata,
-    Column('cookie_id', Integer(), primary_key=True),
+    'cookies', metadata, Column('cookie_id', Integer(), primary_key=True),
     Column('cookie_name', String(50), index=True),
-    Column('cookie_recipe_url', String(255)),
-    Column('cookie_sku', String(55)),
-    Column('quantity', Integer()),
-    Column('unit_cost', Numeric(12, 2)),
+    Column('cookie_recipe_url', String(255)), Column('cookie_sku', String(55)),
+    Column('quantity', Integer()), Column('unit_cost', Numeric(12, 2)),
     CheckConstraint('unit_cost >= 0.00', name='unit_cost_positive'),
-    Index('ix_cookies_cookie_name', 'cookie_name')
-)
+    Index('ix_cookies_cookie_name', 'cookie_name'))
+
+orders = Table('orders', metadata,
+               Column('order_id', Integer(), primary_key=True),
+               Column('user_id', ForeignKey('users.user_id')),
+               Column('shipped', Boolean(), default=False))
+
+line_items = Table('line_items', metadata,
+                   Column('line_items_id', Integer(), primary_key=True),
+                   Column('order_id', ForeignKey('orders.order_id')),
+                   Column('cookie_id', ForeignKey('cookies.cookie_id')),
+                   Column('quantity', Integer()),
+                   Column('extended_cost', Numeric(12, 2)))
