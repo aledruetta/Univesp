1a4dd52352eda42334a31b68fb2bb601d3efa7a2
Alejandro Druetta <aledruetta@gmail.com>
2021-04-08 12:45:51 -0300

    SamuraiApp C# EF

---
+++ Banco_de_Dados/Entity_Framework/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+    }
+}
---
+++ Banco_de_Dados/Entity_Framework/SamuraiApp/SamuraiApp.Domain/Quote.cs
@@ -0,0 +1,10 @@
+namespace SamuraiApp.Domain
+{
+    public class Quote
+    {
+        public int Id { get; set; }        
+        public string Text { get; set; }        
+        public Samurai Samurai { get; set; }        
+        public int SamuraiId { get; set; }        
+    }
+}
