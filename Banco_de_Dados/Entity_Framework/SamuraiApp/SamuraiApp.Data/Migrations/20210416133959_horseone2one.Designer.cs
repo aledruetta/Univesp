dba99e34ce6cecec363830fff371e78517f0b679
Alejandro Druetta <aledruetta@gmail.com>
2021-04-16 10:41:53 -0300

    One to one relationship

--- Banco_de_Dados/Entity_Framework/SamuraiApp/SamuraiApp.Data/Migrations/20210416133959_horseone2one.Designer.cs
+++ Banco_de_Dados/Entity_Framework/SamuraiApp/SamuraiApp.Data/Migrations/20210416133959_horseone2one.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SamuraiApp.Data;
 
 namespace SamuraiApp.Data.Migrations
 {
     [DbContext(typeof(SamuraiContext))]
-    partial class SamuraiContextModelSnapshot : ModelSnapshot
+    [Migration("20210416133959_horseone2one")]
+    partial class horseone2one
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Banco_de_Dados/Entity_Framework/SamuraiApp/SamuraiApp.Domain/Samurai.cs
+++ Banco_de_Dados/Entity_Framework/SamuraiApp/SamuraiApp.Domain/Samurai.cs
@@ -8,5 +8,6 @@ namespace SamuraiApp.Domain
         public string Name { get; set; }
         public List<Quote> Quotes { get; set; } = new List<Quote>();
         public List<Battle> Battles { get; set; } = new List<Battle>();
+        public Horse Horse { get; set; }
     }
 }
