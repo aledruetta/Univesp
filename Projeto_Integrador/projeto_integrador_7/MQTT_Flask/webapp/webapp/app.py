ad9ee11d17ba7aea9dcc555827a7750b35364aa7
Alejandro Druetta <aledruetta@gmail.com>
2021-05-25 16:34:19 -0300

    Fix editorconfig

--- Projeto_Integrador/projeto_integrador_7/MQTT_Flask/webapp/webapp/app.py
+++ Projeto_Integrador/projeto_integrador_7/MQTT_Flask/webapp/webapp/app.py
@@ -1,26 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_mqtt import Mqtt
 
+from webapp.ext import mqtt
 
-mqtt = Mqtt()
-
-@mqtt.on_connect()
-def handle_connect(client, userdata, flags, rc):
-    mqtt.subscribe('esp32/test')
-
-@mqtt.on_message()
-def handle_mqtt_message(client, userdata, message):
-    data = dict(
-        topic=message.topic,
-        payload=message.payload.decode()
-    )
-    print(data)
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config.from_object("webapp.config.DevConfig")
+
     mqtt.init_app(app)
 
     return app
