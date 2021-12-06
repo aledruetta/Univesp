a39107f714d602449e5d05feb00aa9bfa7999ac2
Alejandro Druetta <aledruetta@gmail.com>
2021-12-06 10:14:57 -0300

    crear componente

---
+++ Angular/Udemy/Curso_Desarrollo_Web_Angular_11/MiPrimeraApp/src/app/saludo/saludo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saludo',
+  templateUrl: './saludo.component.html',
+  styleUrls: ['./saludo.component.css']
+})
+export class SaludoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
