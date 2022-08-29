ce9ec7f83183db97e2cc3b246eebea0712903bdb
Alejandro Druetta <aledruetta@gmail.com>
2022-08-29 09:29:14 -0300

    Verification

---
+++ Arquitetura_Computadores/Nand2Tetris/07/VMTranslator/output/StackTest/StackTest.asm
@@ -0,0 +1,342 @@
+// push constant 17
+@17
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 17
+@17
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// eq
+@SP
+AM=M-1
+D=M
+A=A-1
+D=D-M
+D=-D
+M=-1
+@JEQ_1
+D;JEQ
+@SP
+A=M-1
+M=0
+(JEQ_1)
+// push constant 17
+@17
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 16
+@16
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// eq
+@SP
+AM=M-1
+D=M
+A=A-1
+D=D-M
+D=-D
+M=-1
+@JEQ_2
+D;JEQ
+@SP
+A=M-1
+M=0
+(JEQ_2)
+// push constant 16
+@16
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 17
+@17
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// eq
+@SP
+AM=M-1
+D=M
+A=A-1
+D=D-M
+D=-D
+M=-1
+@JEQ_3
+D;JEQ
+@SP
+A=M-1
+M=0
+(JEQ_3)
+// push constant 892
+@892
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 891
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// lt
+@SP
+AM=M-1
+D=M
+A=A-1
+D=D-M
+D=-D
+M=-1
+@JLT_4
+D;JLT
+@SP
+A=M-1
+M=0
+(JLT_4)
+// push constant 891
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 892
+@892
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// lt
+@SP
+AM=M-1
+D=M
+A=A-1
+D=D-M
+D=-D
+M=-1
+@JLT_5
+D;JLT
+@SP
+A=M-1
+M=0
+(JLT_5)
+// push constant 891
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 891
+@891
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// lt
+@SP
+AM=M-1
+D=M
+A=A-1
+D=D-M
+D=-D
+M=-1
+@JLT_6
+D;JLT
+@SP
+A=M-1
+M=0
+(JLT_6)
+// push constant 32767
+@32767
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 32766
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// gt
+@SP
+AM=M-1
+D=M
+A=A-1
+D=D-M
+D=-D
+M=-1
+@JGT_7
+D;JGT
+@SP
+A=M-1
+M=0
+(JGT_7)
+// push constant 32766
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 32767
+@32767
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// gt
+@SP
+AM=M-1
+D=M
+A=A-1
+D=D-M
+D=-D
+M=-1
+@JGT_8
+D;JGT
+@SP
+A=M-1
+M=0
+(JGT_8)
+// push constant 32766
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 32766
+@32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// gt
+@SP
+AM=M-1
+D=M
+A=A-1
+D=D-M
+D=-D
+M=-1
+@JGT_9
+D;JGT
+@SP
+A=M-1
+M=0
+(JGT_9)
+// push constant 57
+@57
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 31
+@31
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 53
+@53
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+@SP
+AM=M-1
+D=M
+A=A-1
+M=D+M
+// push constant 112
+@112
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+AM=M-1
+D=-M
+A=A-1
+M=D+M
+// neg
+@SP
+A=M-1
+M=-M
+// and
+@SP
+AM=M-1
+D=M
+A=A-1
+M=D&M
+// push constant 82
+@82
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// or
+@SP
+AM=M-1
+D=M
+A=A-1
+M=D|M
+// not
+@SP
+A=M-1
+M=!M
