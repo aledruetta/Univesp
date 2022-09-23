7fa4119c4be14bc4170d8f9aa462cdb010bc7a63
Alejandro Druetta <aledruetta@gmail.com>
2022-09-23 09:12:44 -0300

    subst2

--- Programacao_Funcional/Scheme/the_little_schemer.scm
+++ Programacao_Funcional/Scheme/the_little_schemer.scm
@@ -62,6 +62,16 @@
     (lambda (new old lat) 
         (if (null? lat) '()
             (cond
-                ((eq? old (car lat)) (cons new (cdr lat)))
-                (else (cons (car lat) (subst new old (cdr lat))))
+                ((eq? old (car lat)) (cons new (cdr lat)) )
+                (else (cons (car lat) (subst new old (cdr lat)) ))
+))))
+
+; (subst2 3 1 2 '(1 2 3 4)) -> (3 2 3 4)
+
+(define subst2
+    (lambda (new o1 o2 lat)
+        (if (null? lat) '()
+            (cond
+                ((or (eq? o1 (car lat)) (eq? o2 (car lat)) ) (cons new (cdr lat)) )
+                (else (cons (car lat) (subst2 new o1 o2 (cdr lat)) ))
 ))))
