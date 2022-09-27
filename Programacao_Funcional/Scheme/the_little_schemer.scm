02334089c15ca4bed6815d3257ae07c52f3120bf
Alejandro Druetta <aledruetta@gmail.com>
2022-09-27 08:24:23 -0300

    multirember and multiinsertr

--- Programacao_Funcional/Scheme/the_little_schemer.scm
+++ Programacao_Funcional/Scheme/the_little_schemer.scm
@@ -75,3 +75,23 @@
                 ((or (eq? o1 (car lat)) (eq? o2 (car lat)) ) (cons new (cdr lat)) )
                 (else (cons (car lat) (subst2 new o1 o2 (cdr lat)) ))
 ))))
+
+; (multirember 1 '(1 2 3 1 5 1)) -> (2 3 5)
+
+(define multirember
+    (lambda (a lat)
+        (if (null? lat) '()
+            (cond
+                ((eq? (car lat) a) (multirember a (cdr lat)))
+                (else (cons (car lat) (multirember a (cdr lat))))
+))))
+
+; (multiinsertR 3 1 '(1 2 3 1 5 1)) -> (1 3 2 3 1 3 5 1 3)
+
+(define multiinsertR 
+    (lambda (new old lat)
+        (if (null? lat) '()
+        (cond
+            ((eq? (car lat) old) (cons old (cons new (multiinsertR new old (cdr lat)))))
+            (else (cons (car lat) (multiinsertR new old (cdr lat))))
+))))
