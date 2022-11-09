2a731d9a43d5359f1d49172c2c6dc5b7e2705f86
Alejandro Druetta <aledruetta@gmail.com>
2022-11-09 16:18:28 -0300

    Variables

--- Linguagens_de_Programacao/Rust/ultimate_rust_crash_course/exercise/a_variables/variables/src/main.rs
+++ Linguagens_de_Programacao/Rust/ultimate_rust_crash_course/exercise/a_variables/variables/src/main.rs
@@ -1,10 +1,15 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
 fn main() {
-    let mut missiles = 8;
-    let ready = 2;
+    // let mut missiles: i32 = STARTING_MISSILES;
+    // let ready: i32 = READY_AMOUNT;
+    // let (mut missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
+    let (missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
 
     println!("Firing {} of my {} missiles...", ready, missiles);
 
-    missiles = missiles - ready;
+    // missiles = missiles - ready;
 
-    println!("{} missiles left", missiles);
+    println!("{} missiles left", missiles - ready);
 }
