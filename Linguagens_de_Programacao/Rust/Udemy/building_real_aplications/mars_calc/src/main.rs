44b757b26b431feeccd71ae189475f6c6e5bf0ab
Alejandro Druetta <aledruetta@gmail.com>
2022-12-12 09:31:26 -0300

    Updates

--- Linguagens_de_Programacao/Rust/Udemy/building_real_aplications/mars_calc/src/main.rs
+++ Linguagens_de_Programacao/Rust/Udemy/building_real_aplications/mars_calc/src/main.rs
@@ -1,27 +1,19 @@
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
     let mut input = String::new();
 
+    print!("Enter you weight (kg)? ");
+    io::stdout().flush().unwrap();
     io::stdin()
-        .read_line(&mut input);
+        .read_line(&mut input)
+        .unwrap();
 
-    borrow_string(&input);
-    own_string(input);
-
-    // println!("Input: {}", input);
-    // let mars_weight = calculate_weight_on_mars(78.0);
-    // println!("Weight on Mars: {}gr", mars_weight * 1000.0);
+    let weight: f32 = input.trim().parse().unwrap();
+    let mars_weight = calculate_weight_on_mars(weight);
+    println!("Weight on Mars: {}kg", mars_weight);
 }
 
 fn calculate_weight_on_mars(earth_weigth: f32) -> f32 {
     (earth_weigth / 9.81) * 3.711
-}
-
-fn own_string(s: String) {
-    println!("{}", s);
-}
-
-fn borrow_string(s: &String) {
-    println!("{}", s);
 }
