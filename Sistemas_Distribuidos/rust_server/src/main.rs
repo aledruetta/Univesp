4bb65af7169c5c05006cc3499e5fcb373da5f3ac
Alejandro Druetta <aledruetta@gmail.com>
2022-12-14 08:39:07 -0300

    structs

---
+++ Sistemas_Distribuidos/rust_server/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr
+        }
+    }
+
+    fn run(self) {
+    }
+}
