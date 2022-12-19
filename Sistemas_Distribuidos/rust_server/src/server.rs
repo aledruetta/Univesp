0c0b0797b4e6119666e813787d02784a170e316e
Alejandro Druetta <aledruetta@gmail.com>
2022-12-19 09:25:12 -0300

    Updates

--- Sistemas_Distribuidos/rust_server/src/server.rs
+++ Sistemas_Distribuidos/rust_server/src/server.rs
@@ -1,3 +1,5 @@
+use std::{net::TcpListener, io::Read};
+
 pub struct Server {
     addr: String,
 }
@@ -8,6 +10,18 @@ impl Server {
     }
 
     pub fn run(self) {
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
         println!("Listening on {}...", self.addr);
+
+        loop {
+            match listener.accept() {
+                Err(e) => println!("Failed to establish a connection: {}", e),
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    stream.read(&mut buffer);
+                },
+            }
+        };
     }
 }
