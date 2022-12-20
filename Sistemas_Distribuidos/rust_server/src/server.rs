ff9a004d73f3b31a1805475c0a86e3272a511c73
Alejandro Druetta <aledruetta@gmail.com>
2022-12-20 17:27:35 -0300

    updates

--- Sistemas_Distribuidos/rust_server/src/server.rs
+++ Sistemas_Distribuidos/rust_server/src/server.rs
@@ -1,4 +1,8 @@
-use std::{net::TcpListener, io::Read};
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+use std::io::Read;
+use std::net::TcpListener;
 
 pub struct Server {
     addr: String,
@@ -16,12 +20,23 @@ impl Server {
 
         loop {
             match listener.accept() {
-                Err(e) => println!("Failed to establish a connection: {}", e),
                 Ok((mut stream, _)) => {
                     let mut buffer = [0; 1024];
-                    stream.read(&mut buffer);
-                },
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {
+
+                                },
+                                Err(e) => println!("Failed to parse a request: {}", e)
+                            }
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to establish a connection: {}", e),
             }
-        };
+        }
     }
 }
