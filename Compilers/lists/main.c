9278caed0059f9132e439add4dddcc91f6753fa4
Alejandro Druetta <aledruetta@gmail.com>
2022-12-27 06:49:41 -0300

    updates

--- Compilers/lists/main.c
+++ Compilers/lists/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// Types
 
 typedef struct ListNode ListNode;
-typedef ListNode * SimpleList;
-
-struct ListNode
+typedef struct ListNode
 {
     char * value;
     ListNode * next;
-};
+} * SimpleList;
+
+// Prototypes
+
+void sl_tostring(SimpleList);
+void sl_append(SimpleList, char*, size_t);
+void sl_drop(SimpleList);
+
+// Functions
 
 void sl_tostring(SimpleList head)
 {
@@ -21,13 +30,21 @@ void sl_tostring(SimpleList head)
     printf("\n");
 }
 
-void sl_append(SimpleList head, ListNode * node)
+void sl_append(SimpleList head, char value[], size_t value_len)
 {
     ListNode * last = head;
     while (last->next != NULL) {
-        last = head->next;
+        last = last->next;
     }
+
+    ListNode * node = (ListNode*) malloc(sizeof(ListNode));
+    char * node_value = (char*) malloc(value_len);
+    strncpy(node_value, value, 2);
+    node->value = node_value;
+    node->next = NULL;
     last->next = node;
+
+    sl_tostring(head);
 }
 
 void sl_drop(SimpleList head)
@@ -42,31 +59,19 @@ void sl_drop(SimpleList head)
     free(last);
 }
 
-ListNode * sl_create_node(char * value)
-{
-    ListNode * node = (ListNode*) malloc(sizeof(ListNode));
-    node->value = value;
-    node->next = NULL;
-    printf("node created: {%s, %p}\n", node->value, node->next);
-
-    return node;
-}
-
 int main()
 {
-    SimpleList sl = sl_create_node("head");
+    ListNode head = { "head", NULL };
+    SimpleList sl = &head;
 
-    for (char c='a'; c<='z'; c++) {
+    for (char c='a'; c<='f'; c++) {
         char str[] = { c, '\0'};
-        ListNode * node = sl_create_node(str);
-        sl_append(sl, node);
+        sl_append(sl, str, 2);
     }
-    /*
 
-    sl_tostring(sl);
+    //sl_tostring(sl);
 
     sl_drop(sl);
-    */
 
     return 0;
 }
