70a4cf2c587c63c8bee97aca18d9b7a4365c70de
Alejandro Druetta <aledruetta@gmail.com>
2023-01-03 09:36:10 -0300

    update two sum

--- Programming_Languages/Rust/exercises/two_sum/src/main.rs
+++ Programming_Languages/Rust/exercises/two_sum/src/main.rs
@@ -1,16 +1,18 @@
 fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let len = nums.len();
     let mut res: Vec<i32> = vec![];
+    let mut index1: usize = 0;
+    let mut slice = &nums[..];
 
-    'outer: for n in 0..len {
-        for m in (n + 1)..len {
-            if nums[n] + nums[m] == target {
-                res.push(n as i32);
-                res.push(m as i32);
-
-                break 'outer;
-            }
+    while slice.len() >= 2 {
+        let diff = target - slice[0];
+        slice = &nums[index1 + 1..];
+        if slice.contains(&diff) {
+            let index2 = slice.iter().position(|&x| x == diff).unwrap() + index1 + 1;
+            res.push(index1 as i32);
+            res.push(index2 as i32);
+            break;
         }
+        index1 += 1;
     }
 
     res
@@ -18,4 +20,6 @@ fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 
 fn main() {
     println!("{:?}", two_sum(vec![2, 7, 11, 15], 9));
+    println!("{:?}", two_sum(vec![3, 2, 4], 6));
+    println!("{:?}", two_sum(vec![3, 3], 6));
 }
