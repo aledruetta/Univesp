61484bbe8cec733928c6dfe0d9564d2217957b9c
Alejandro Druetta <aledruetta@gmail.com>
2023-03-17 11:51:27 -0300

    update convertidor

--- Programming_Languages/Angular/Udemy/Angular14GuiaCompleta/Convertidor/src/app/Models/moneda.ts
+++ Programming_Languages/Angular/Udemy/Angular14GuiaCompleta/Convertidor/src/app/Models/moneda.ts
@@ -1,4 +1,14 @@
+const USD_ARS: number = 202.94;
+const USD_BRL: number = 5.24;
+const BRL_ARS: number = 38.73;
+
 export class Moneda {
+
+  static factors = {
+    'ARS': [1, 1/USD_ARS, 1/BRL_ARS],
+    'USD': [USD_ARS, 1, USD_BRL],
+    'BRL': [BRL_ARS, 1/USD_BRL, 1]
+  };
 
   code: string;
   order: number;
--- Programming_Languages/Angular/Udemy/Angular14GuiaCompleta/Convertidor/src/app/components/convertidor/convertidor.component.ts
+++ Programming_Languages/Angular/Udemy/Angular14GuiaCompleta/Convertidor/src/app/components/convertidor/convertidor.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { Moneda } from 'src/app/Models/moneda';
 
-const USD_ARS: number = 202.94;
-const USD_BRL: number = 5.24;
-const BRL_ARS: number = 38.73;
-
 @Component({
   selector: 'app-convertidor',
   templateUrl: './convertidor.component.html',
@@ -13,13 +9,13 @@ const BRL_ARS: number = 38.73;
 export class ConvertidorComponent {
 
   monedas = [
-    new Moneda('ARS', 0, [1, 1/USD_ARS, 1/BRL_ARS]),
-    new Moneda('USD', 1, [USD_ARS, 1, USD_BRL]),
-    new Moneda('BRL', 2, [BRL_ARS, 1/USD_BRL, 1])
+    new Moneda('ARS', 0, Moneda.factors['ARS']),
+    new Moneda('USD', 1, Moneda.factors['USD']),
+    new Moneda('BRL', 2, Moneda.factors['BRL'])
   ];
 
-  tengoTipo: Moneda = <Moneda>this.monedas.find(m => m.order === 0);
-  quieroTipo: Moneda = <Moneda>this.monedas.find(m => m.order === 1);
+  tengoTipo: Moneda = <Moneda>this.monedas.find(m => m.code === 'ARS');
+  quieroTipo: Moneda = <Moneda>this.monedas.find(m => m.code === 'USD');
 
   tengo: number = 0;
   quiero: number = 0;
