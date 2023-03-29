16541d4372a2e1c2a43a91740081f1760e41f5f8
Alejandro Druetta <aledruetta@gmail.com>
2023-03-29 12:02:44 -0300

    update noticias

--- Programming_Languages/Angular/Udemy/Angular14GuiaCompleta/noticias/src/app/app.component.ts
+++ Programming_Languages/Angular/Udemy/Angular14GuiaCompleta/noticias/src/app/app.component.ts
@@ -19,7 +19,6 @@ export class AppComponent {
     this._noticiasService.getNews(criteriosBusqueda)
       .subscribe((resultado) => {
         this.noticias = this._noticiasService.mapNewsFromArticles(resultado.articles);
-        console.log(this.noticias);
       }
     );
   }
